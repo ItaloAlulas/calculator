2c681826a5c0a495f3197720e78c97cc41f80a22
ItaloAlulas <183663705+ItaloAlulas@users.noreply.github.com>
2025-07-17 14:27:20 -0300

    Calculator wih ready-to-use functionality and basic style

--- script.js
+++ script.js
@@ -11,25 +11,31 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return "ERROR";
+    }
+
     return num1 / num2;
 }
 
 function operate(num1, operator, num2) {
     let result;
 
-    switch (operator) {
+    switch (operator.trim()) {
         case "+":
             result = add(num1, num2);
             return result;
-        case "-":
+        case "—":
             result = subtract(num1, num2)
             return result;
-        case "×":
+        case "X":
             result = multiply(num1, num2)
             return result;
         case "÷":
             result = divide(num1, num2);
             return result;
+        default:
+            return "ERROR";
     }
 }
 
@@ -45,6 +51,19 @@ const digitButtons = document.querySelectorAll(".digitButton");
 // Changes the display content when pressed a digit button
 digitButtons.forEach(button => {
     button.addEventListener("click", () => {
+        if (displayContent.textContent === "0" && button.textContent === "0") { return; };
+
+        if (displayContent.textContent === "0" && button.textContent != "0") {
+            num1 = button.textContent;
+            console.log(num1);
+            displayContent.textContent = button.textContent;
+            return;
+        };
+
+        if (displayContent.textContent === "ERROR") {
+            displayContent.textContent = "";
+            num1 = "";
+        };
         if (operatorClicked === false) {
             num1 += button.textContent;
             console.log(`num1: ${num1}`);
@@ -57,36 +76,119 @@ digitButtons.forEach(button => {
     });
 });
 
+// "." button
+const dotButton = document.querySelector(".dotButton");
+dotButton.addEventListener("click", () => {
+    if (displayContent.textContent === "ERROR") {
+        displayContent.textContent = "";
+    }
+
+    if (!operatorClicked) {
+        if (num1.includes(".") === false) {
+            if (num1 === "" && displayContent.textContent === "") {
+                num1 = "0.";
+                displayContent.textContent += "0.";
+
+            } else if (num1 === "" && displayContent.textContent === "0") {
+                num1 += "0.";
+                displayContent.textContent += ".";
+            } else {
+                num1 += ".";
+                displayContent.textContent += ".";
+            }
+        }
+    } else {
+        if (num2.includes(".") === false) {
+            if (num2 === "") {
+                num2 = "0.";
+                displayContent.textContent += "0.";
+            } else {
+                num2 += ".";
+                displayContent.textContent += ".";
+            }
+        }
+    }
+});
+
 // Saves the operator button content in the "operator" variable
 const operatorButtons = document.querySelectorAll(".operatorButton");
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        operatorClicked = operatorClicked === false ? true : false;
+        if (num1 === "") return;
+
+        operatorClicked = true;
+
+        num2 = "";
         displayContent.textContent = "";
         console.log(operatorClicked);
+
         operator = button.textContent;
-    })
+    });
 });
 
+// "=" button
 const equalButton = document.querySelector(".equalButton");
 equalButton.addEventListener("click", () => {
-    operatorClicked = operatorClicked === false ? true : false;
+    if (!operatorClicked) return;
+
+    if (num2 === "") {
+        num1 = "";
+        operatorClicked = false;
+        displayContent.textContent = "ERROR";
+        return
+    }
+
+    operatorClicked = false;
     console.log(operatorClicked);
-    num2 = parseFloat(num2);
-    result = operate(num1, operator, num2);
-    displayContent.textContent = `${result}`;
-    num1 = result;
+
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+
+    let result = operate(n1, operator, n2);
+
+    if (result === "ERROR" || isNaN(result)) {
+        displayContent.textContent = "ERROR";
+        num1 = "";
+        num2 = "";
+        operator = "";
+        return;
+    }
+
+    if (Number.isInteger(result)) {
+        displayContent.textContent = result.toString();
+    } else {
+        displayContent.textContent = parseFloat(result.toFixed(3)).toString();
+    }
+
+    num1 = result.toString();
     num2 = "";
     console.log(`num1: ${num1}`);
-})
+});
 
+// "C" button
 const clearButton = document.querySelector("#clearButton");
 clearButton.addEventListener("click", () => {
-    displayContent.textContent = "";
+    displayContent.textContent = "0";
 
-    if (operatorClicked === false) {
-        num1 = 0;
+    num1 = "";
+    num2 = "";
+    operator = "";
+    result = "";
+    console.log(`num1: ${num1}`);
+
+    operatorClicked = false;
+});
+
+// "DEL button"
+const deleteButton = document.querySelector("#deleteButton");
+deleteButton.addEventListener("click", () => {
+    if (displayContent.textContent === "ERROR") return;
+
+    if (!operatorClicked) {
+        num1 = num1.slice(0, -1);
+        displayContent.textContent = num1 || 0;
     } else {
-        num2 = 0;
+        num2 = num2.slice(0, -1);
+        displayContent.textContent = num2 || 0;
     }
 });
